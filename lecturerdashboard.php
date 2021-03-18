5c0034a8242492839656fef568ac6e5f7cb0848c
Enomartins <enowebservices@gmail.com>
2021-03-18 22:37:12 +0100

    fixed UI and content and created questions and comment pages

--- lecturerdashboard.php
+++ lecturerdashboard.php
@@ -44,9 +44,39 @@ include("auth_session.php");
     <link href="stylee.css?<?=filemtime("stylee.css")?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
+
+    <div class="container-fluid bg-transparent w-100 p-0">
+            <nav style="background-color: purple;" class="navbar navbar-expand-md navbar-light px-0 w-100 px-5">
+                <a style="font-size: 2rem" class="navbar-brand px-4 display-7" href="#">SchoolApp</a>
+                <button class="navbar-toggler mr-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
     
-    <div class="container mt-5">
-    <h1>Lecturer</h1>
+                    
+    
+                    <li class="nav-item font-weight-bold">
+                      <a class="nav-link" href="login.php">DASHBOARD</a>
+                    </li>
+                    
+                    <li class="nav-item mx-5">
+                      <a style="background-color:purple; color:white;" class="font-weight-bold nav-link btn"  href="logout.php">LOGOUT</a>
+                    </li>
+                  </ul>
+                  
+                </div>
+              </nav>
+        </div>
+    
+    <div class="container pt-5 mt-5 mx-5">
+    <div class="row my-5 pt-5">
+    <h1 class="mt-5 pt-5">Lecturer Dashboard</h1>
+    </div>
         <div class="row top">
             <div class="col-sm-3">
                 <div class="card">
--- studentdashboard.php
+++ studentdashboard.php
@@ -29,6 +29,34 @@ if (isset($_REQUEST['course'])) {
     <link href="stylee.css?<?=filemtime("stylee.css")?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
+
+<div class="container-fluid bg-transparent w-100 p-0">
+            <nav style="background-color: purple;" class="navbar navbar-expand-md navbar-light px-0 w-100 px-5">
+                <a style="font-size: 2rem" class="navbar-brand px-4 display-7" href="#">SchoolApp</a>
+                <button class="navbar-toggler mr-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+    
+                    
+    
+                    <li class="nav-item font-weight-bold">
+                      <a class="nav-link" href="login.php">DASHBOARD</a>
+                    </li>
+                    
+                    <li class="nav-item mx-5">
+                      <a style="background-color:purple; color:white;" class="font-weight-bold nav-link btn"  href="logout.php">LOGOUT</a>
+                    </li>
+                  </ul>
+                  
+                </div>
+              </nav>
+        </div>
     
     <div class="container mt-4">
     <h1>Student</h1>
@@ -76,11 +104,11 @@ if (isset($_REQUEST['course'])) {
                         {
                         $result->data_seek($j);
                         $row = $result->fetch_array(MYSQLI_ASSOC);
-                        echo "<div class=d-flex><h5 class='mx-4'>".$row['title'] ."</h5>
+                        echo "<div class='card'><div class='my-5 card-body'><h5 class='card-title'>".$row['title'] ."</h5>
                         <form class='form' action='' method='post'>
                         <input type='hidden' id='' name='course' value='". $row['title']. "'>
                         <input type='submit' name='submit' value='Add course' class='login-button'>
-                        </form></div>
+                        </form></div></div>
                         ". "<br>";
                         }
             ?>
