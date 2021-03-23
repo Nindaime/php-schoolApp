9580f43b69863ea0b9c93f7a8aa92cf6341d3a28
Enomartins <enowebservices@gmail.com>
2021-03-23 11:02:49 +0100

    fixed title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Martins</title>
+    <title>SCHOOL-APP</title>
     <link href="bootstrap.min.css?<?=filemtime("bootstrap.min.css")?>" rel="stylesheet" type="text/css" />
     <link href="stylee.css?<?=filemtime("stylee.css")?>" rel="stylesheet" type="text/css" />
 </head>
